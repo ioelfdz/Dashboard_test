6aa88c5fdeb6dd5d671346ee1790c5c2c7546a0b
ioelfdz <ioelfdz@yahoo.com>
2024-09-22 15:02:06 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ def img_to_base64(image_path):
         with open(image_path, "rb") as img_file:
             return base64.b64encode(img_file.read()).decode()
     except Exception as e:
-        logging.error(f"Error converting image to base64: {str(e)}")
+        print(f"Error converting image to base64: {str(e)}")
+        #logging.error(f"Error converting image to base64: {str(e)}")
         return None
 
 # Set the title and favicon that appear in the Browser's tab bar.
