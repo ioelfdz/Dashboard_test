a78cd43fe9c5ce6b9e021fc530ffd983749425e4
ioelfdz <ioelfdz@yahoo.com>
2024-09-22 02:15:11 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ with st.sidebar:
         options = ["SWBD","LVDO","MC-MCAR","SureSeT","Jobs Journey Map"],
         icons = ["activity","activity","activity","activity", "activity"],
         menu_icon = "cast",
-        #menu_icon = logo,
+        sidebarlogo = Image.open('logo')
         default_index = 0,
         #orientation = "horizontal",
 )
