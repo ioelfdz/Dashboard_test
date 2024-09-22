bbd006ef02851955c73e40217d4657c1755bf656
ioelfdz <ioelfdz@yahoo.com>
2024-09-22 01:52:37 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ logo = Path(__file__).parent/'data/o4a_logo.png'
 #df_2 = pd.read_csv(DATA_FILENAME_2)
 
 with st.sidebar:
-    image = "logo"
+    st.image(logo, width=100)
     selected = option_menu(
         menu_title = "Main Menu",
         options = ["SWBD","LVDO","MC-MCAR","SureSeT","Jobs Journey Map"],
